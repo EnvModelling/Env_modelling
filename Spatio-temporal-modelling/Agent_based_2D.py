9f05940bef491f1f0ba8fbbccfde9ef49f079e59
Paul Connolly <p.connolly@manchester.ac.uk>
2021-11-08 20:28:09 +0000

    added basic python scripts

---
+++ Spatio-temporal-modelling/Agent_based_2D.py
@@ -0,0 +1,215 @@
+import matplotlib
+import matplotlib.pyplot as plt #as per the above, much easier to write over and over again
+from numpy import random
+from matplotlib import colors
+from IPython.display import clear_output, display
+import pdb
+import random
+import numpy as np
+
+# class, defining agents with their position and group membership
+class agent:
+    def __init__(self,x,y,group):
+        self.score = 100 # agent's life score
+        self.x = x
+        self.y = y
+        self.group = group
+        
+# define a function for creating agents and assigning them to grid
+def agentCreator(size,group,groupList,field,n,m):
+    # loop through entire group
+    for j in range(0,size):
+        # select random available location 
+        while True:
+            # random x coordinate
+            x = random.choice(range(0,n))
+            # random y coordinate
+            y = random.choice(range(0,m))
+            # check if spot is available; if not then re-iterate 
+            if field[x][y] == None:
+                field[x][y] = agent(x=x,y=y,group=group)
+                # append agent object reference to group list
+                groupList.append(field[x][y])
+                # exit while loop; spot on field is taken
+                break
+
+# function for creating an initial grid
+def initfield(populationSizeA,populationSizeB):
+    # initializing new empty grid, using list comprehension in Python
+    field_grid = [[None for i in range(0,100)] for j in range(0,100)]
+    # create empty list for containing agent references in future, type A & B
+    agents_A = []
+    agents_B = []
+    # assigning random spots to agents of group A and B; 
+    agentCreator(size = populationSizeA,
+                    group = "A",
+                    groupList = agents_A,
+                    field = field_grid,
+                    n = 100,
+                    m = 100)
+    agentCreator(size = populationSizeB,
+                    group = "B",
+                    groupList = agents_B,
+                    field = field_grid,
+                    n = 100,
+                    m = 100)
+
+    # return populated grid and a list of agents in groups A and B
+    return field_grid, agents_A, agents_B
+
+# executing above function for a population size of 1000 for both groups
+field, agents_A, agents_B = initfield(populationSizeA=1000,populationSizeB=700)
+# print (np.count_nonzero(np.array(mapfield(field))==2.))
+
+# Now lets plot the 2D space and see what our initial condition looks like
+def mapfield(field):
+    #.imshow() needs a matrix with float elements;
+    population = [[0.0 for i in range(0,100)] for j in range(0,100)]
+    # if agent is of type A, put a 1.0, if of type B, pyt a 2.0
+    for i in range(0,100):
+        for j in range(0,100):
+            if field[i][j] == None: # empty
+                pass # leave 0.0 in population cell
+            elif field[i][j].group == "A": # group A agents
+                population[i][j] = 1.0 # 1.0 means "A"
+            else: # group B agents
+                population[i][j] = 2.0 # 2.0 means "B"
+    # return mapped values
+    return population
+
+population_grid = mapfield(field)
+
+colormap = colors.ListedColormap(["white","green","blue"])
+
+fig, ax = plt.subplots(figsize=(10,10))
+ax.imshow(population_grid, cmap=colormap)
+plt.title('Agent distribution: Group A green, B blue')
+plt.ion()
+plt.show()
+
+
+
+
+# function for removing agents from battlefield grid when life score is not strictly positive
+def removeAgents(field):
+    # identifying agents with life score of score or below - and removing them from the grid
+    for i in range(0,len(field)):
+        for j in range(0,len(field)):
+            if field[i][j]:
+                if field[i][j].score <= 0:
+                    # remove this agent since life score is not strictly positive
+                    field[i][j] = None
+        
+# function implementing one round of interaction, for an agent of type A
+def oneRoundAgentA(i,j,attackRange):
+    found_i = None
+    found_j = None
+    # look in neigbouring cells in same order for each iteration
+    list1 = [k for k in range(i-attackRange,i+attackRange+1)]
+    list2 = [l for l in range(j-attackRange,j+attackRange+1)]
+#     random.shuffle(list1)
+#     random.shuffle(list2)
+    for k in list1:
+        for l in list2:
+            # check for negative index values; if so - break!
+            if k < 0 or l < 0:
+                break
+                # check for index values above 99, if so break!
+            if k > 99 or l > 99:
+                break
+            if field[k][l]:
+                if field[k][l].group == "B": # then this is an enemy
+                    if found_i == None:
+                        found_i = k
+                        found_j = l
+                    
+    # deal damage to identified specie
+    if found_i != None:
+        field[found_i][found_j].score = field[found_i][found_j].score - random.randint(10,60)
+        
+# function implementing one round of interaction, for an agent of type B
+def oneRoundAgentB(i,j,attackRange):
+    found_i = None
+    found_j = None
+    # look in neigbouring cells in same order for each iteration
+    
+    # In B's case we look randomly through the surrounding space
+    list1 = [k for k in range(i-attackRange,i+attackRange+1)]
+    list2 = [l for l in range(j-attackRange,j+attackRange+1)]
+    random.shuffle(list1)
+    random.shuffle(list2)
+    for k in list1:
+        for l in list2:
+            # check for negative index values; if so - break!
+            if k < 0 or l < 0:
+                break
+                # check for index values above 99, if so break!
+            if k > 99 or l > 99:
+                break
+            if field[k][l]:
+                if field[k][l].group == "A": # then this is an enemy
+                    if found_i == None:
+                        found_i = k
+                        found_j = l
+    # deal damage to identified species
+    if found_i != None:
+        field[found_i][found_j].score = field[found_i][found_j].score - random.randint(10,60)
+        
+        
+        
+import time
+
+record_field = np.zeros((100,100,51),dtype=float)
+population_grid = mapfield(field)
+record_field[:,:,0] = population_grid
+
+for counter in range(1,51): # in this case I am conducting 50 iterations 
+     
+    # iterating through all cells on the battlefield
+    for x in range(0,100):
+        for y in range(0,100):
+            # print("top tier iteration, i: "+str(i)+", j: "+str(j))
+            # check if there is an agent within the respective cell
+            if field[x][y] != None:
+                # depending on the type: execute respective attack strategy
+                if field[x][y].group == "A":
+                    # one round of battle for this agent of type A
+                    oneRoundAgentA(i = x, j = y,attackRange=10)
+                else: 
+                    # one round of battle for this agent of type B
+                    oneRoundAgentB(i = x, j = y,attackRange=10)
+    # identifying agents with life score of score or below - and removing them from the grid
+    removeAgents(field)
+    population_grid = mapfield(field)
+    record_field[:,:,counter] = population_grid
+
+    
+fig, ax = plt.subplots(figsize=(10,10))
+plt.imshow(record_field[:,:,0], cmap=colormap)
+plt.title('Agent distribution: Group A green, B blue')
+plt.show()
+
+# for counter in range(0,50):
+#     plt.rcParams["figure.figsize"] = (10, 10)
+#     #display(plt.gcf())
+#     clear_output(wait=True)
+#     plt.imshow(record_field[:,:,counter], cmap=colormap)
+#     plt.show();
+#     time.sleep(0.05);
+        
+        
+A_list = np.zeros((51), dtype=int)
+B_list = np.zeros((51), dtype=int)
+time_array = np.linspace(0.0, 51.0, num=51)
+
+for counter in range(0,51):
+    A_list[counter]=np.count_nonzero(record_field[:,:,counter] == 1)
+    B_list[counter]=np.count_nonzero(record_field[:,:,counter] == 2)
+    
+fig, ax = plt.subplots(figsize=(7,7))
+plt.plot(time_array, A_list, 'g', label='specie A',linewidth=2)
+plt.plot(time_array, B_list, 'b', label='specie B',linewidth=2)
+plt.xlabel('time', fontsize=12)
+plt.ylabel('species concentration', fontsize=16)
+plt.legend()
+plt.show()
---
+++ Spatio-temporal-modelling/Reaction_diffusion_2D.py
@@ -0,0 +1,245 @@
+import numpy as np
+from IPython.display import clear_output, display
+import matplotlib.pyplot as plt #as per the above, much easier to write over and over again 
+import time
+
+def discrete_laplacian(M):
+    """Get the discrete Laplacian of matrix M"""
+    L = -4*M
+    L += np.roll(M, (0,-1), (0,1)) # right neighbor
+    L += np.roll(M, (0,+1), (0,1)) # left neighbor
+    L += np.roll(M, (-1,0), (0,1)) # top neighbor
+    L += np.roll(M, (+1,0), (0,1)) # bottom neighbor
+    
+    return L
+
+def gray_scott_update(A, B, DA, DB, f, k, delta_t):
+    """
+    Updates a concentration configuration according to a Gray-Scott model
+    with diffusion coefficients DA and DB, as well as feed rate f and
+    kill rate k.
+    """
+    
+    # Let's get the discrete Laplacians first
+    LA = discrete_laplacian(A)
+    LB = discrete_laplacian(B)
+    
+    # Now apply the update formula
+    diff_A = (DA*LA - A*B**2 + f*(1-A)) * delta_t
+    diff_B = (DB*LB + A*B**2 - (k+f)*B) * delta_t
+    
+    A += diff_A
+    B += diff_B
+    
+    return A, B
+    
+def get_initial_configuration(N, random_influence=0.2):
+    """
+    Initialize a concentration configuration. N is the side length
+    of the (N x N)-sized grid.
+    `random_influence` describes how much noise is added.
+    """
+    
+    # We start with a configuration where on every grid cell 
+    # there's a lot of chemical A, so the concentration is high
+    A = (1-random_influence) * np.ones((N,N)) + random_influence * np.random.random((N,N))
+    
+    # Let's assume there's only a bit of B everywhere
+    B = random_influence * np.random.random((N,N))
+    
+    # Now let's add a disturbance in the center
+    N2 = N//2
+    radius = r = int(N/10.0)
+    
+    #A[N2-r:N2+r, N2-r:N2+r] = 0.50
+    #B[N2-r:N2+r, N2-r:N2+r] = 0.25
+    
+    # PJC
+    x=np.linspace(-100,100,N) 
+    X,Y=np.meshgrid(x,x)    
+    radius= np.sqrt(X*X+Y*Y) 
+    ind=np.where(radius<10)
+    A[ind]=1.
+    B[ind]=0.0                                                                          
+    
+    return A, B
+    
+A, B = get_initial_configuration(200)
+
+fig, ax = plt.subplots(1,2,figsize=(12,12))
+first=ax[0].imshow(A, cmap='Greys',vmin=0, vmax=1)
+second=ax[1].imshow(B, cmap='Greys',vmin=0, vmax=1)
+ax[0].set_title('A')
+ax[1].set_title('B')
+ax[0].axis('off')
+ax[1].axis('off')
+fig.colorbar(first,ax=ax[0],fraction=0.046, pad=0.04)
+fig.colorbar(first,ax=ax[1],fraction=0.046, pad=0.04)
+plt.ion()
+plt.show()
+plt.close()
+
+
+# update in time
+delta_t = 1.0
+
+# Diffusion coefficients
+DA = 0.16
+DB = 0.08
+
+DA = 0.14
+DB = 0.06
+
+
+# define feed/kill rates
+f = 0.060
+k = 0.062
+
+k = 0.065
+
+# grid size
+N = 200
+
+# simulation steps
+N_simulation_steps = 10000
+
+
+A, B = get_initial_configuration(200)
+
+for t in range(N_simulation_steps):
+    A, B = gray_scott_update(A, B, DA, DB, f, k, delta_t)
+    
+fig, ax = plt.subplots(1,2,figsize=(12,12))
+ax[0].imshow(A, cmap='Greys',vmin=0, vmax=1)
+ax[1].imshow(B, cmap='Greys',vmin=0, vmax=1)
+ax[0].set_title('A')
+ax[1].set_title('B')
+ax[0].axis('off')
+ax[1].axis('off')
+fig.colorbar(first,ax=ax[0],fraction=0.046, pad=0.04)
+fig.colorbar(first,ax=ax[1],fraction=0.046, pad=0.04)
+plt.ion()
+plt.show()
+plt.close()
+
+# We can check the values for both A and B 
+print("Total amount of A = ", np.sum(A)) 
+print("Total amount of B = ", np.sum(B))
+
+
+# Experiment 2
+
+# Create a 2D array of f values that increasesd as we move away from the centre.
+x_values = np.linspace(-50.0, 50.0, num=200)
+y_values = np.linspace(-50.0, 50.0, num=200)
+z_values = np.zeros((200,200))
+x_index=0
+for x in x_values:
+    y_index=0
+    for y in y_values:
+        z_values[x_index,y_index] = (np.exp(np.abs(x/50)*np.abs(y/50)))
+        y_index+=1
+    x_index+=1
+
+# Diffusion coefficients
+DA = 0.16
+DB = 0.08
+
+# define feed/kill rates
+f = 0.030
+k = 0.062
+f_values = z_values*f
+    
+# Please insert the code required to run a simulation now using the variable 'f_values'
+
+A, B = get_initial_configuration(200)
+
+for t in range(N_simulation_steps):
+    A, B = gray_scott_update(A, B, DA, DB, f_values, k, delta_t)
+    
+fig, ax = plt.subplots(1,2,figsize=(12,12))
+ax[0].imshow(A, cmap='Greys',vmin=0, vmax=1)
+ax[1].imshow(B, cmap='Greys',vmin=0, vmax=1)
+ax[0].set_title('A')
+ax[1].set_title('B')
+ax[0].axis('off')
+ax[1].axis('off')
+fig.colorbar(first,ax=ax[0],fraction=0.046, pad=0.04)
+fig.colorbar(first,ax=ax[1],fraction=0.046, pad=0.04)
+plt.show()
+plt.close()
+
+
+
+# Experiment 3
+# Diffusion coefficients
+DA = 0.2
+DB = 0.1
+
+# define feed/kill rates
+f = 0.035
+k = 0.062
+
+# mitosis
+f=0.0367
+k=0.0649
+
+
+
+N_simulation_steps = 4000
+
+delta_t = 1.0
+
+# intialize the figures
+A, B = get_initial_configuration(200)
+
+fig, ax = plt.subplots(figsize=(12,12))
+plt.imshow(A, cmap='Greys',vmin=0, vmax=1)
+plt.colorbar(fraction=0.046, pad=0.04)
+plt.show()
+plt.close()
+
+# Let us also store the total concentration of A and B as a function of time for an extra 
+# plot after this simulation,
+
+import os
+import getpass
+
+username=getpass.getuser()
+
+A_conc_list=[]
+B_conc_list=[]
+time_list=[]
+it=0
+for i in range(N_simulation_steps):
+    time_list.append(i)
+    plt.rcParams["figure.figsize"] = (12, 12);
+    #display(plt.gcf());
+    clear_output(wait=True)
+    A_new, B_new = gray_scott_update(A, B, DA, DB, f, k, delta_t)
+    A_conc_list.append(np.sum(A_new))
+    B_conc_list.append(np.sum(B_new))
+    A, B = A_new, B_new
+    
+    if(np.mod(i,50)==0):
+        it=it+1
+        if(i==0):
+            myobj=plt.imshow(A, cmap='Greys',vmin=0, vmax=1)
+            #plt.colorbar(fraction=0.046, pad=0.04)
+            #plt.show();
+        else:
+            myobj.set_data(A)
+            
+        myobj.set_clim((0.,1.))
+        
+        if not os.path.exists('/tmp/' + username):
+            os.mkdir('/tmp/' + username)
+        if it==0:
+            os.system('rm /tmp/' + username + '/*')
+        
+        plt.savefig('/tmp/' +username + '/frame%03d.png' % it,format='png') 
+
+os.system('convert -delay 20 /tmp/'  + username +'/frame*.png /tmp/' +username + '/animation.gif')
+os.system('rm /tmp/' + username + '/frame*.png')
+
+        
